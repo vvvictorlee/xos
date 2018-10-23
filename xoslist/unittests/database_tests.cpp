008e952b7d1113fd91184826245cb80eaa818a4d
victorylee <lisheng2007@hotmail.com>
2018-10-23 22:23:41 +0800

    Update xoslist/unittests/database_tests.cpp

--- xoslist/unittests/database_tests.cpp
+++ xoslist/unittests/database_tests.cpp
@@ -6,8 +6,10 @@
 #include <eosio/testing/tester.hpp>
 #include <eosio/chain/global_property_object.hpp>
 #include <fc/crypto/digest.hpp>
-
+#include <boost/container/flat_set.hpp>
+#include <eosio/chain/types.hpp>
 #include <boost/test/unit_test.hpp>
+#include <boost/algorithm/string.hpp>
 
 #ifdef NON_VALIDATING_TEST
 #define TESTER tester
@@ -22,33 +24,78 @@ namespace bfs = boost::filesystem;
 
 BOOST_AUTO_TEST_SUITE(database_tests)
 
-   // Simple tests of undo infrastructure
-   BOOST_AUTO_TEST_CASE(undo_test) {
+flat_set<account_name> parse_list_string(string items)
+{
+  flat_set<account_name> item_list;
+vector<string> itemlist;
+      boost::split(itemlist, items, boost::is_any_of(","));
+      for (string item: itemlist) {
+      
+        //  try {
+        //     node = boost::lexical_cast<uint16_t,string>(node_number);
+        //  }
+        //  catch(boost::bad_lexical_cast &) {
+        //     // This exception will be handled below
+        //  }
+        //  if (node < 0 || node > 99) {
+        //     cerr << "Bad node number found in node number list: " << node_number << endl;
+        //     exit(-1);
+        //  }
+         item_list.insert(string_to_name(item.c_str()));
+      }
+
+      return item_list;
+}
+
+
+    // Simple tests of undo infrastructure
+   BOOST_AUTO_TEST_CASE(list_config_parse_test) {
       try {
          TESTER test;
+      //  // Bypass read-only restriction on state DB access for this unit test which really needs to mutate the DB to properly conduct its test.
+      //    eosio::chain::database& db = const_cast<eosio::chain::database&>( test.control->db() );
 
-         // Bypass read-only restriction on state DB access for this unit test which really needs to mutate the DB to properly conduct its test.
-         eosio::chain::database& db = const_cast<eosio::chain::database&>( test.control->db() );
+      //    auto ses = db.start_undo_session(true);
 
-         auto ses = db.start_undo_session(true);
+      //    // Create an account
+      //    db.create<global_property_list_object>([](global_property_list_object &a) {
+      //            chain_list_config config;
+      //            config.resource_whitelist={1};
+      //      a.set_list(config);
+      //    });
 
-         // Create an account
-         db.create<account_object>([](account_object &a) {
-            a.name = "billy";
-         });
+      //    // Make sure we can retrieve that account by name
+      //    const global_property_list_object& ptr = db.get<global_property_list_object>();
+      //   //   BOOST_TEST(a.list_configuration.size > 0);
+      //    chain_list_config a  = ptr.get_list();
+       
+      //    uint64_t v = 0;
+      //    if(a.resource_whitelist.size() > 0)
+      //    {
+      //          v =  *(a.resource_whitelist.begin());
+      //    }
+      string str= "alice,bob,tom";
+      flat_set<account_name> list = parse_list_string(str);
+         BOOST_TEST(list.size() > 0);
+         account_name n =N(a);
+         if(list.size()>0)
+         {
+            n = *(list.begin());
+         }
 
-         // Make sure we can retrieve that account by name
-         auto ptr = db.find<account_object, by_name, std::string>("billy");
-         BOOST_TEST(ptr != nullptr);
-
+         BOOST_TEST(n !=N(a));
+         BOOST_TEST(n ==N(a));
+          BOOST_TEST(n ==N(alice));
+        //  BOOST_TEST(v == 1);
          // Undo creation of the account
-         ses.undo();
+        //  ses.undo();
 
-         // Make sure we can no longer find the account
-         ptr = db.find<account_object, by_name, std::string>("billy");
-         BOOST_TEST(ptr == nullptr);
+        //  // Make sure we can no longer find the account
+        //  ptr = db.find<global_property_list_object, by_name, std::string>("billy");
+        //  BOOST_TEST(ptr == nullptr);
       } FC_LOG_AND_RETHROW()
    }
+
  // Simple tests of undo infrastructure
    BOOST_AUTO_TEST_CASE(list_config_test) {
       try {
@@ -85,6 +132,36 @@ BOOST_AUTO_TEST_SUITE(database_tests)
         //  BOOST_TEST(ptr == nullptr);
       } FC_LOG_AND_RETHROW()
    }
+   
+   // Simple tests of undo infrastructure
+   BOOST_AUTO_TEST_CASE(undo_test) {
+      try {
+         TESTER test;
+
+         // Bypass read-only restriction on state DB access for this unit test which really needs to mutate the DB to properly conduct its test.
+         eosio::chain::database& db = const_cast<eosio::chain::database&>( test.control->db() );
+
+         auto ses = db.start_undo_session(true);
+
+         // Create an account
+         db.create<account_object>([](account_object &a) {
+            a.name = "billy";
+         });
+
+         // Make sure we can retrieve that account by name
+         auto ptr = db.find<account_object, by_name, std::string>("billy");
+         BOOST_TEST(ptr != nullptr);
+
+         // Undo creation of the account
+         ses.undo();
+
+         // Make sure we can no longer find the account
+         ptr = db.find<account_object, by_name, std::string>("billy");
+         BOOST_TEST(ptr == nullptr);
+      } FC_LOG_AND_RETHROW()
+   }
+
+
    // Test the block fetching methods on database, fetch_bock_by_id, and fetch_block_by_number
    BOOST_AUTO_TEST_CASE(get_blocks) {
       try {
